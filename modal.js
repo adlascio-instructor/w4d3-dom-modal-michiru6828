8882cf0281d559bdc8fb54940989fb69fb1551e1
michiru6828 <since_l999@yahoo.co.jp>
2023-03-01 14:28:46 -0800

    done

--- modal.js
+++ modal.js
@@ -1,9 +1,20 @@
-console.log("here");
+// console.log("here");
 
 // select button element
+const openButton = document.querySelector("#openModal");
 
 // add event listener on click to button
+openButton.addEventListener("click", () => {
+    document.querySelector("#modal").classList.remove("invisible");
+});
+
 // remove class invisible from div modal
 
 // add event listener on key press Escape
 // add class invisible to div modal
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+        document.querySelector("#modal").classList.add("invisible")
+    }
+})
